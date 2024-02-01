8d5fdb7c942406bed37fc36b4e0439f5ea1e05dd
Jarreth Santos <santos.jarreth@gmail.com>
2024-01-31 18:39:27 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+let employee = {
+
+name: "Jarreth",
+streetAdress: "122 Capricorn",
+
+}
+
+function updateEmployeeWithKeyAndValue (object, key, value) {
+    const newObject = {...object};
+
+    newObject[key] = value;
+
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (object, key, value) {
+    object[key] = value;
+
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key, value) {
+    const newObject = {...object};
+    //newObject[key] = value;
+
+    delete newObject[key];
+    return newObject;
+      // delete object[key];
+       // return object;
+}
+
+function destructivelyDeleteFromEmployeeByKey (object, key, value) {
+    const newObject = {...object};
+    //newObject[key] = value;
+
+    delete object[key];
+    return object;
+}
